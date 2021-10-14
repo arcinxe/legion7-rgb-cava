eea5840a510c657bb15c6aa1b8a3a4353bf1d88a
Marcin Arkan <marcinxe@gmail.com>
2021-10-14 06:53:01 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import * as Functions from './functions'
+import * as Types from './types'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as yargs from 'yargs'
+
+const options = yargs  
+  .argv
+
+
+
+
---
+++ src/functions.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as cliProgress from 'cli-progress'
+import * as Types from './types'
